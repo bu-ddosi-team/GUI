efdfed1eae327e5d529a303c535512022caa1ac4
cekchian <cekchian@bu.edu>
2014-03-30 15:02:14 -0400

    Ability to Set Controls

---
+++ SD GUI/SeniorDesign_1/ProfileNameWindow.h
@@ -0,0 +1,112 @@
+#pragma once
+
+namespace SeniorDesign_1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for ProfileNameWindow
+	/// </summary>
+	public ref class ProfileNameWindow : public System::Windows::Forms::Form
+	{
+	public:
+		//declare a flag variable to indicate whether the settings should be saved to the file
+		int flagSave;
+		ProfileNameWindow(void)
+		{
+			InitializeComponent();
+			flagSave = 0;
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~ProfileNameWindow()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label1;
+	protected: 
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Button^  button1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 23);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(98, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Enter Profile Name:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(116, 23);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(218, 20);
+			this->textBox1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(140, 59);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Save";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &ProfileNameWindow::button1_Click);
+			// 
+			// ProfileNameWindow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(366, 94);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Name = L"ProfileNameWindow";
+			this->Text = L"ProfileNameWindow";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+				 this->Hide();
+				 flagSave = 1;
+			 }
+	public: System::String^ getProfileName(){
+				String^ ProfileName =  this->textBox1->Text;
+				//clear text from textbox in PopUp window
+				this->textBox1->Clear();
+				return ProfileName;
+			}
+	};
+}
